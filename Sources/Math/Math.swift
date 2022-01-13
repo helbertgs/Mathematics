02b0aaa06d53020962b3e0d1227bb0d4b12c2e3a
Helbert Emilio Gomes <helbertgs@gmail.com>
2022-01-13 03:03:49 -0300

    feat: create abs function to return the absolute value from a number or vector

--- Sources/Math/Math.swift
+++ Sources/Math/Math.swift
@@ -1,6 +1,15 @@
-public struct Math {
-    public private(set) var text = "Hello, World!"
+import Swift
 
-    public init() {
-    }
+/// For vectors, the returned vector contains the absolute value of each element of the input vector.
+/// - Parameter a: Vector or scalar of which to determine the absolute value.
+/// - Returns: Returns absolute value of scalars and vectors.
+@inlinable public func abs(_ a: Double) -> Double {
+    a < 0 ? -a : a
+}
+
+/// For vectors, the returned vector contains the absolute value of each element of the input vector.
+/// - Parameter a: Vector or scalar of which to determine the absolute value.
+/// - Returns: Returns absolute value of scalars and vectors.
+@inlinable public func abs(_ a: Vector) -> Vector {
+    .init(x: abs(a.x), y: abs(a.y), z: abs(a.z))
 }
