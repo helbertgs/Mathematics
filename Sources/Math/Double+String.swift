202d1959ea26234b9719482a970ecd36e3d7e649
Helbert Emilio Gomes <helbertgs@gmail.com>
2022-01-16 00:19:59 -0300

    chore: update extension

--- Sources/Math/Double+String.swift
+++ Sources/Math/Double+String.swift
@@ -2,7 +2,7 @@ import Swift
 
 extension Double {
     @inlinable
-    public func round(_ places: Int) -> Double {
+    public func rounded(_ places: Int) -> Double {
         let divisor = pow(10.0, Double(places))
         return (self * divisor).rounded() / divisor
     }
--- Tests/MathTests/MathTests.swift
+++ Tests/MathTests/MathTests.swift
@@ -3,8 +3,8 @@ import XCTest
 
 final class MathTests: XCTestCase {
     func testArccosineOfZero() throws {
-        let theta = arccos(0).round(3)
-        let expected = (Double.pi / 2).round(3)
+        let theta = arccos(0).rounded(3)
+        let expected = (Double.pi / 2).rounded(3)
         XCTAssertEqual(theta, expected)
     }
 
@@ -13,8 +13,8 @@ final class MathTests: XCTestCase {
     }
 
     func testArccosineOfHalf() throws {
-        let theta = arccos(0.5).round(3)
-        let expected = (Double.pi / 3).round(3)
+        let theta = arccos(0.5).rounded(3)
+        let expected = (Double.pi / 3).rounded(3)
         XCTAssertEqual(theta, expected)
     }
 }
